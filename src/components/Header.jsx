6fb9764851f9bf95c379f50c8f13a21baf0eac50
Ajith-M-001 <ajithguru741@gmail.com>
2024-01-09 17:32:32 +0530

    29 modify Header to display user

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,33 @@
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
 const Header = () => {
+  const { userInfo } = useSelector((state) => state.user);
   return (
     <div className="bg-gray-600 text-white p-4">
       <div className="flex justify-between items-center container mx-auto">
         <Link to={"/"}>
           <h1 className="text-3xl font-bold">MERN-AUTH</h1>
         </Link>
-        <nav className="space-x-5 font-semibold text-lg">
-          <Link to={"/login"} className="hover:underline">
-            Login
-          </Link>
-          <Link to={"/register"} className="hover:underline">
-            Register
-          </Link>
-        </nav>
+        {userInfo ? (
+          <div className="flex items-center space-x-2">
+            <p>{userInfo.name}</p>
+            <img
+              className="w-10 h-10 object-cover rounded-full cursor-pointer"
+              src={userInfo.image}
+              alt="profile pic"
+            />
+          </div>
+        ) : (
+          <nav className="space-x-5 font-semibold text-lg">
+            <Link to={"/login"} className="hover:underline">
+              Login
+            </Link>
+            <Link to={"/register"} className="hover:underline">
+              Register
+            </Link>
+          </nav>
+        )}
       </div>
     </div>
   );
